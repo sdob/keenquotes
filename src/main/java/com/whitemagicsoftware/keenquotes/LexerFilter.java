7928edf0e22a211d05a914a728bbba59d607d356
Dave Jarvis <Dave.Jarvis@gmail.com>
2022-08-13 14:54:50 -0700

    Remove unused class

--- src/main/java/com/whitemagicsoftware/keenquotes/LexerFilter.java
+++
@@ -1,7 +0,0 @@
-package com.whitemagicsoftware.keenquotes;
-
-import java.text.CharacterIterator;
-import java.util.function.Predicate;
-
-public interface LexerFilter extends Predicate<CharacterIterator> {
-}
